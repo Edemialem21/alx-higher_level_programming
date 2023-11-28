55d7a73641017049c5190c99fb7019b62350ca37
Edemialem21 <21edemialem@gmail.com>
2023-11-28 15:20:50 +0300

    python functiono that does the power of a number

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    po = 1
-    if b == 0:
-        return 1
-    elif b < 0:
-        for i in range(abs(b)):
-            po /= a
-        return po
-    else:
-        for i in range(b):
-            po *= a
-        return po           
+    return (a ** b)
