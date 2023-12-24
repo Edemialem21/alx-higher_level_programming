608b7e9142ea81b8e28ad1e3b8872fafbeb7dc0f
Edemialem21 <21edemialem@gmail.com>
2023-12-24 22:40:08 +0300

    all about lists

---
+++ Python@Neso_Academy/data-stracture.py
@@ -0,0 +1,6 @@
+# lists - is a collection of similar or different types of data items.
+#       - is created by placing itemes inside [] separeted by commas.
+#       - numbers = [] --is an empty list.
+# a list can have a duplicate items.
+# items in a list are mutable.
+# lists can have items of differents data types.
