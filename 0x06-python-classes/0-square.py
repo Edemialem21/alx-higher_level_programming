a9f3686d76e05db54e9224973c1e3a68de87f6b4
Edemialem21 <21edemialem@gmail.com>
2023-12-31 17:03:00 +0300

    pyton class definiation with documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+
 """
 File: 0-square.py
-Desc: this is defining an empty class
+Desc: contains an empty class defination.
+Author: Edemialem
+Date Created: December 31 2023
 """
+
 
 class Square():
     """
-    defining an empty class named Square
+    This is an empty classs with named Square.
     """
-
     pass
