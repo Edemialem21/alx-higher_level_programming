534a91bb22c82c4386c12bbf95a05ab0c0197370
Edemialem21 <21edemialem@gmail.com>
2023-12-31 16:55:00 +0300

    pyton class definiation with documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-""" 
-File: 0-square
-Desc: this is defining an empty class, named with Square() so as to
-show how to add documentation
+"""
+File: 0-square Desc: this is defining an empty class, named with Square() so as to show how to add documentation
 """
 
 class Square():
