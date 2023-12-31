9641424ffd4c3b39d1019a58329604988ac859c9
Edemialem21 <21edemialem@gmail.com>
2023-12-31 15:18:43 +0300

    python class and object

--- Python@Neso_Academy/data-stracture.py
+++ Python@Neso_Academy/data-stracture.py
@@ -4,3 +4,19 @@
 # a list can have a duplicate items.
 # items in a list are mutable.
 # lists can have items of differents data types.
+
+# accessing elements of a single dimensional list
+# multi-dimensional list - list containg another list
+# my_list = [[1, 2, 3], "edemialem"]
+# 
+
+# adding elements to a list
+# -append() , insert(), extend()
+# my_list.append(value)  -  at the end.
+# my_list.append([value1, value]) -- a list can be added as an item.
+# my_list.insert(position, value) -- to add an item at a specifi positon.
+# my_list.extend(list2) - used to add all items of one ist in another list.
+
+# input() - used to take input from the usser
+#          - usser input is always converted to aas string
+# number int(input("Enter the number: "))
---
+++ Python@Neso_Academy/oop.py
@@ -0,0 +1,14 @@
+#  object-oriented programming
+# involves programming using objects. represtnt real world object.
+# an object has a state, a behaviour and an identity
+# examples: student circle button bank account
+# behaviour of an object- defined by the methods of aan objeect
+# behaviours - actions, to invoke or call a method on an object is to ask the object to performn an action .
+# str.charAt(2), 
+
+# Identity of an object - what makes an object uniqe
+
+
+# Jenny's lectures in oop.
+# why we need oop?
+# 
