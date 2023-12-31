e4616b994d206dced05b9b83c02ce256b587d77a
Edemialem21 <21edemialem@gmail.com>
2023-12-31 17:12:48 +0300

    creating class with private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+contains a class defination with private instance intialization using __self.
+"""
+
+class Square():
+    """
+    This is a classs with named Square with creating private instance of size.
+    """
+
+    def __init__(self, size):
+        self.__size = size
