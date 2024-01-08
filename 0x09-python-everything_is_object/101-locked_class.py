125dd0440e9d31ee2aa7c38356ec3b39fd8c3e6f
Edemialem21 <21edemialem@gmail.com>
2024-01-08 11:14:58 +0300

    fix bug

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+
+"""
+File: 101-locked_class.py
+Desc: This module contains a single class defination.
+"""
+
+
 class LockedClass():
+    """
+    This class prevents the user from dynamically creating
+    new instance attributes
+    """
     __slots__ = ["first_name"]
