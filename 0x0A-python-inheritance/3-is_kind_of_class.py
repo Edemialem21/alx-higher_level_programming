359823a2b517a233e986083d50ee971d08649c9d
Edemialem21 <21edemialem@gmail.com>
2024-01-10 22:22:09 +0300

    king of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Desc: This file contains a single function defination
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    This function returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited
+    from, the specified class ; otherwise False.
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    return False
