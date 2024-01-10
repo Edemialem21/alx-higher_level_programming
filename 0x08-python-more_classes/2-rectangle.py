61f3e4de4173cdc029a4f7dbabc862f50b461d4e
Edemialem21 <21edemialem@gmail.com>
2024-01-10 11:12:43 +0300

    including public instance method to th rectangle class

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -57,4 +57,4 @@ class Rectangle:
         """calculates perimeter of the rectangle"""
         if self.width == 0 or self.height == 0:
             return 0
-        return ((2 * self.width) + (2 * self.height))
+        return (2 * (self.width + self.height))
