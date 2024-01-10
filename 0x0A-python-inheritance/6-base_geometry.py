a53fc5d5ed883ba8329a12a9137af5475ae7b257
Edemialem21 <21edemialem@gmail.com>
2024-01-10 22:28:04 +0300

    bug fixed

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Desc: This module defines an empty class.
+"""
+
+
+class BaseGeometry:
+    """
+    A simple class defination.
+    """
+    def area(self):
+        """
+        A function that raises an Exception.
+        """
+        raise Exception("area() is not implemented")
