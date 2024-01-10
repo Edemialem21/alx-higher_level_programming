2ff0eb7dfa9c331c749abab65e077ced61413385
Edemialem21 <21edemialem@gmail.com>
2024-01-10 10:16:25 +0300

    class of recatangle with width and height instance with its own methods

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+creating an empty class called Rectangle
+"""
+
+
+class Rectangle:
+    """
+    an empty class of rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """ initialze all private instance with default values"""
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        """getter to retrive the width"""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        """
+        set the width valu except for it is not integer and less than zero
+        if that is not the case it raise typerror and value error
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self._width = value
+
+    @property
+    def height(self):
+        """it gets the height value"""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        """
+        set the hight valu except for it is not integer and less than zero
+        if that is not the case it raise typerror and value error
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self._height = value
