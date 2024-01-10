5e2c07f0726df3b50d01804d840fc4a85af0585f
Edemialem21 <21edemialem@gmail.com>
2024-01-10 22:24:04 +0300

    bug fixed

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Desc: This file contains a single function defination.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    This function  returns True if the object is an
+    instance of a class that inherited (directly or indirectly)
+    from the specified class ; otherwise False.
+    """
+
+    if isinstance(obj, a_class) and type(obj) != a_class:
+        return True
+    return False
