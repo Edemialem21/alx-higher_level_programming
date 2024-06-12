bd582bdaf97006ce111883de5c3a7e5870534a22
Edemialem21 <21edemialem@gmail.com>
2024-06-12 15:12:44 +0300

    create a new user and grant all the privilage

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- This script creates a new user with all privilages
+-- script that  creates a new user with all privilages
 CREATE USER IF NOT EXISTS user_0d_1@localhost;
 SET PASSWORD FOR user_0d_1@localhost = 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
