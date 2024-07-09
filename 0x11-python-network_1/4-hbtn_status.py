407e47c746d9526e0b89aeea6a4ab9b6925c577a
Edemialem21 <21edemialem@gmail.com>
2024-07-09 19:37:28 +0300

    fecthes

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a python script  that fetches https://alx-intranet.hbtn.io/status"""
+from requests import get
+
+
+if __name__ == "__main__":
+    result = get("https://alx-intranet.hbtn.io/status").text
+    print("Body response:")
+    print("\t- type: {}".format(type(result)))
+    print("\t- content: {}".format(result))
